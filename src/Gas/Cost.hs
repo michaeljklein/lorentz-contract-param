1a2a96df35716004fd637538db036131ec090405
rvem <roman.melnikov@serokell.io>
2019-07-25 17:01:14 +0300

    fixup! [TM-80] Brush up

--- src/Gas/Cost.hs
+++ src/Gas/Cost.hs
@@ -1,9 +1,16 @@
-
-module Gas.Cost where
+-- | Module with gas cost calculation helpers that are used
+-- in the other modules. Ported from here:
+-- https://github.com/BrianGuo/MichelsonExecutionEngine/blob/master/util/gas_costs.ml#L8
+module Gas.Cost
+  ( bytes
+  , string
+  , zint
+  )where
 
 import Gas.Type
 import qualified Gas.Script as Script
 
+bytes, string, zint :: Word64 -> Cost
 bytes  = allocMBytesCost
 string = allocBytesCost
 zint   = allocBitsCost . Script.significantBitCount
